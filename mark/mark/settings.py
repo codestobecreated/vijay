b69a48ffc9f936fe5b18cf2558bf81ed15fe2d16
codestobecreated <vijaykumarpalanisamy2001@gmail.com>
2025-03-19 09:17:26 -0700

    Ready for Render deployment

--- mark/mark/settings.py
+++ mark/mark/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#0g-v&_^z99@pelhjalqft=5iooh4m^a(104(fy@4xr(z16m$('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
